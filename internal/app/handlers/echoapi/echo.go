48dcd9709e7aed71bb08176888f23965088cd686
Vadim Alekseev <vadimaleksv@gmail.com>
2021-09-07 16:01:04 +0300

    Add zerolog

--- internal/app/handlers/echoapi/echo.go
+++ internal/app/handlers/echoapi/echo.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/rs/zerolog/log"
+
 	echov1 "github.com/aleksvdim/go-grpc-gateway-template/pkg/echo/v1"
 )
 
@@ -11,6 +13,7 @@ import (
 func (e EchoAPI) Echo(ctx context.Context, request *echov1.EchoRequest) (*echov1.EchoResponse, error) {
 	repoEcho, err := e.repo.Echo(ctx, request.Message)
 	if err != nil {
+		log.Err(err).Msg("error executing Echo repository method")
 		return nil, fmt.Errorf("error getting echo from server: %s", err)
 	}
 
